c38fc2f287e36eab15f476e8b2ec0b32a05fcc89
Cainã Andrade Fernandes da Rocha <caina.fernandes@academico.ifrn.edu.br>
2024-12-05 08:28:04 -0300

    shearch Box Done.ok

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,31 +2,57 @@ import { useContext } from "react";
 import Product from "./Product";
 import { CircularProgress } from "@mui/material";
 import { CartContext } from "../context/CartContext";
+import { Search } from "@mui/icons-material";
 
 export default function Shop() {
 
     const { products, loading, error } = useContext(CartContext);
 
-    return (
-        <section id="shop">
-            <h2>Elegant Products for Everyone</h2>
 
-            <ul id="products">
-                {error && <p>{error}</p>}
+    useEffect(() => {
+        if(products) {
+setFilteredItems(products);
+        }
+    }, [products]);
+
+ function handleSearch() {
+    const term = SearchInput.current.value.toLowerCase();
+    setFilteredItems(
+
+
+        function clearSearch() {
+            SearchInput.current.value = "";
+            setFilteredItems(products);
+        }
+    )
+ }
+    return (
+        <ul id="products">
+             {error && <p>{error}</p>}
                 {!loading && products ? (
                     products.map((product) => (
-                        <li key={product.id}>
-                            <Product {...product} />
-                        </li>
-                    ))
+                        {loading &&
+                            <div id="loading">
+                            <CircularProgress...</p>
+                            </div>
+                        }
+                        {!loading && !error && FilteredItems.length > 0 ? (
+                            FilteredItems.map((product) => (
+                                <li key={product.id}>
+                                    <Product {...product} />
+                                </li>
+                            ))
                 ) : (
                     <div id="loading">
-                        <CircularProgress size="10rem" color="inherit" />
-                        <p>Loading products...</p>
+                    <CircularProgress size="10rem"
+                    color="inherit" />
+                    <p>loading products...</p>
                     </div>
+                    <p>Not found!</p>
                 )}
-            </ul>
+                </ul>
 
-        </section>
-    );
-}
+                </Selection>
+            );
+        }
+                  
